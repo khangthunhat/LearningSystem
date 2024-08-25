fd943ac4d0e2604598a976cc398b432a7c5f788d
khangthunhat <ndinhkhang329@gmail.com>
2024-08-25 16:15:25 +0700

    u

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,133 @@
+"use strict";
+import { Request, Response, NextFunction } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncError";
+import ErrorHandler from "../utils/ErrorHandler";
+import userModel from "../models/user.model";
+import jwt from "jsonwebtoken";
+import ejs from "ejs";
+import path from "path";
+import sendMail from "../utils/sendEmail";
+
+// Register user
+interface IRegistrationBody {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+}
+
+export const registrationUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, password } = req.body as IRegistrationBody;
+
+      const isEmailExist = await userModel.findOne({ email });
+      if (isEmailExist) {
+        return next(new ErrorHandler("Email already exists", 400));
+      }
+
+      const user: IRegistrationBody = {
+        name,
+        email,
+        password,
+      };
+
+      const activationToken = createActivationToken(user);
+
+      const activationCode = activationToken.activationCode;
+
+      const data = { user: { name: user.name }, activationCode };
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/activation-mail.ejs"),
+        data
+      );
+
+      try {
+        await sendMail({
+          email: user.email,
+          subject: "Thư mời dự lễ tốt nghiệp",
+          template: "activation-mail.ejs",
+          data,
+        });
+
+        res.status(201).json({
+          success: true,
+          message: `Please check your email: ${user.email} to activate your account!`,
+          activationToken: activationToken.token,
+        });
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+interface IActivationToken {
+  token: string;
+  activationCode: string;
+}
+
+export const createActivationToken = (
+  user: IRegistrationBody
+): IActivationToken => {
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString(); // 4 digit code
+
+  const token = jwt.sign(
+    {
+      user,
+      activationCode,
+    },
+    process.env.ACTIVATION_SECRET as Secret,
+    {
+      expiresIn: "5m",
+    }
+  );
+
+  return { token, activationCode };
+};
+
+//activate user
+interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+}
+
+export const activateUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } =
+        req.body as IActivationRequest;
+
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_SECRET as string
+      ) as { user: IUser; activationCode: string };
+
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("Invalid activation code", 400));
+      }
+
+      const { name, email, password } = newUser.user;
+
+      const existsUser = await userModel.findOne({ email });
+
+      if (existsUser) {
+        return next(new ErrorHandler("Email already exists", 400));
+      }
+
+      const user = await userModel.create({
+        name,
+        email,
+        password,
+      });
+
+      res.status(201).json({
+        success: true,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,66 @@
+require("dotenv").config();
+import { Request, Response, NextFunction } from "express";
+import { IUser } from "../models/user.model";
+import { redis } from "./redis";
+
+
+interface ITokenOption {
+  expires: Date;
+  maxAge: number;
+  httpOnly: boolean;
+  sameSite: "lax" | "strict" | "none" | undefined;
+  secure?: boolean;
+}
+
+  //parse enviroment variables to interger with fallback values
+ const accessTokenExprise = parseInt(
+    process.env.ACCESS_TOKEN_EXPRISE || "300",
+    10
+  );
+  const refreshTokenExprise = parseInt(
+    process.env.REFRESH_TOKEN_EXPRISE || "1200",
+    10
+  );
+
+  //options for cookies
+export const accessTokenOptions: ITokenOption = {
+    expires: new Date(Date.now() + accessTokenExprise * 60 * 60 * 1000),
+    maxAge: accessTokenExprise * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "lax",
+  };
+
+export  const refreshTokenOptions: ITokenOption = {
+    expires: new Date(Date.now() + refreshTokenExprise * 24 * 60 * 60 * 1000),
+    maxAge: refreshTokenExprise * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "lax",
+  };
+
+export const sendToken = (user: IUser, statusCode: number, res: Response) => {
+  const accessToken = user.SignAccessToken();
+  const refreshToken = user.SignRefreshToken();
+
+  //upload session to redis
+  try {
+    redis.set(user._id.toString(), JSON.stringify(user));
+  } catch (error) {
+    console.error("Redis Error:", error);
+  }
+
+
+
+  //only set secure to true in production
+  if (process.env.NODE_ENV === "production") {
+    accessTokenOptions.secure = true;
+  }
+
+  res.cookie("access_token", accessToken, accessTokenOptions);
+  res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+  res.status(statusCode).json({
+    success: true,
+    user,
+    accessToken,
+  });
+};
