51632ad335862b55d85e90a8cad9e66e6ebe1bda
khangthunhat <ndinhkhang329@gmail.com>
2024-09-10 15:07:24 +0700

    fix order bug

--- server/src/controllers/order.controller.ts
+++ server/src/controllers/order.controller.ts
@@ -8,21 +8,25 @@ import CourseModel from "../models/course.model";
 import NotificationModel from "../models/notification.model";
 import userModel from "../models/user.model";
 import sendMail from "../utils/sendEmail";
-import ejs from 'ejs';
+import ejs from "ejs";
 import { newOrder, getAllOrdersService } from "../services/order.service";
 import path from "path";
 
 export const createOrder = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const {courseId, paymentInfo} = req.body as IOrder;
+      const { courseId, paymentInfo } = req.body as IOrder;
 
       const user = await userModel.findById(req.user?._id);
 
-      const courseExitInUser = user?.courses.some((course: any) => course.courseId == courseId)
+      const courseExitInUser = user?.courses.some(
+        (course: any) => course.courseId == courseId
+      );
 
       if (courseExitInUser) {
-        return next(new ErrorHandler("You have already purchased this course", 400));
+        return next(
+          new ErrorHandler("You have already purchased this course", 400)
+        );
       }
 
       const course = await CourseModel.findById(courseId);
@@ -34,34 +38,40 @@ export const createOrder = CatchAsyncError(
       const data: any = {
         courseId: course._id,
         userId: user?._id,
-      }
-      
+      };
 
       const mailData = {
-        order:{
-          _id: course._id.toString().slice(0,7),
+        order: {
+          _id: course._id.toString().slice(0, 7),
           name: course.name,
           price: course.price,
-          data: new Date().toLocaleDateString('en-US',{year: 'numeric', month: 'long', day: 'numeric'})
-        }
-      }
+          data: new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }),
+        },
+      };
 
-      const html = await ejs.renderFile(path.join(__dirname,'../mails/order-confirmation.ejs'),{order:mailData});
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/order-confirmation.ejs"),
+        { order: mailData }
+      );
 
       try {
-        if(user){
+        if (user) {
           await sendMail({
             email: user.email,
             subject: "Order Comfirmation",
             template: "order-confirmation.ejs",
-            data: mailData
-          })
+            data: mailData,
+          });
         }
       } catch (error: any) {
-        return next(new ErrorHandler(error.message,500));
+        return next(new ErrorHandler(error.message, 500));
       }
 
-      user?.courses.push({courseId: course?._id});
+      user?.courses.push({ courseId: course?._id });
 
       await user?.save();
 
@@ -71,10 +81,9 @@ export const createOrder = CatchAsyncError(
         message: `You have a new order from ${course?.name}`,
       });
 
-      course.purchases ? course.purchases += 1 : course.purchases;
+      course.purchases ? (course.purchases += 1) : course.purchases;
 
-      newOrder(data,res,next);
-
+      newOrder(data, res, next);
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
     }
